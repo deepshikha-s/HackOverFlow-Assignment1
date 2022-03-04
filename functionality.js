2d3b8790f0550faff41a86619f87cb4ebe915a84
Deepshikha Sinha <i.deepshikha.sinha@gmail.com>
2022-03-05 00:50:21 +0530

    Created Basic Layout

---
+++ functionality.js
@@ -0,0 +1,22 @@
+const r = Math.floor(Math.random()*256);
+const g = Math.floor(Math.random()*256);
+const b = Math.floor(Math.random()*256);
+const colorans = [r,g,b];
+var colorseasy;
+var colorshard
+//for (let i = 0; i<6)
+function start() {
+    //document.getElementById
+}
+
+function easy(){
+
+}
+
+function hard(){
+
+}
+
+function check(){
+
+}
