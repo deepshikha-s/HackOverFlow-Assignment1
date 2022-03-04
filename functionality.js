9c37e00da22daf6558eb18e1120e5c35312359a2
Deepshikha Sinha <i.deepshikha.sinha@gmail.com>
2022-03-05 01:52:05 +0530

    Added Functionality

--- functionality.js
+++ functionality.js
@@ -1,20 +1,28 @@
-const r = Math.floor(Math.random()*256);
-const g = Math.floor(Math.random()*256);
-const b = Math.floor(Math.random()*256);
-const colorans = [r,g,b];
+var colorans;
 var colorseasy;
 var colorshard
-//for (let i = 0; i<6)
+
 function start() {
     //document.getElementById
+    //use innerhtml and create another div messagedisplay
 }
 
 function easy(){
-
+    for (let i = 0; i<3; i++){
+        const r = Math.floor(Math.random()*256);
+        const g = Math.floor(Math.random()*256);
+        const b = Math.floor(Math.random()*256);
+        const color = [r,g,b];
+    }
 }
 
 function hard(){
-
+    for (let i = 0; i<6; i++){
+        const r = Math.floor(Math.random()*256);
+        const g = Math.floor(Math.random()*256);
+        const b = Math.floor(Math.random()*256);
+        const color = [r,g,b];
+    }
 }
 
 function check(){
