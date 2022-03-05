2909f1c9a493b9a287f0b811eb4ec620115fc324
Deepshikha Sinha <i.deepshikha.sinha@gmail.com>
2022-03-05 21:52:30 +0530

    Added Functionality

--- functionality.js
+++ functionality.js
@@ -1,30 +1,63 @@
-var colorans;
-var colorseasy;
-var colorshard
+var colorans = [];
+var colorseasy = [];
+var colorshard = [];
 
 function start() {
-    //document.getElementById
-    //use innerhtml and create another div messagedisplay
+    document.getElementById("message").innerHTML = "Choose a mode to play"
 }
 
 function easy(){
+    document.getElementById("message").innerHTML = "Click on the color which has the same rgb values as mentioned."
     for (let i = 0; i<3; i++){
         const r = Math.floor(Math.random()*256);
         const g = Math.floor(Math.random()*256);
         const b = Math.floor(Math.random()*256);
-        const color = [r,g,b];
+        const color = "rgb(" + r +", " + g + ", " + b +")"
+        colorseasy.push(color);
     }
+    var color1 = colorseasy[0]
+    var color2 = colorseasy[1]
+    var color3 = colorseasy[2]
+    var black = 'rgb(0,0,0)';
+    document.getElementById("Color1").style.backgroundColor = color1;
+    document.getElementById("Color2").style.backgroundColor = color2;
+    document.getElementById("Color3").style.backgroundColor = color3;
+    document.getElementById("Color4").style.backgroundColor = black;
+    document.getElementById("Color5").style.backgroundColor = black;
+    document.getElementById("Color6").style.backgroundColor = black;
+    const ans = Math.floor(Math.random()*3);
+    colorans = colorseasy[ans];
+    check();
 }
 
 function hard(){
+    document.getElementById("message").innerHTML = "Click on the color which has the same rgb values as mentioned."
     for (let i = 0; i<6; i++){
         const r = Math.floor(Math.random()*256);
         const g = Math.floor(Math.random()*256);
         const b = Math.floor(Math.random()*256);
-        const color = [r,g,b];
+        const color = "rgb(" + r +", " + g + ", " + b +")"
+        colorshard.push(color);
     }
+    var color1 = colorshard[0]
+    var color2 = colorshard[1]
+    var color3 = colorshard[2]
+    var color4 = colorshard[3]
+    var color5 = colorshard[4]
+    var color6 = colorshard[5]
+    document.getElementById("Color1").style.backgroundColor = color1;
+    document.getElementById("Color2").style.backgroundColor = color2;
+    document.getElementById("Color3").style.backgroundColor = color3;
+    document.getElementById("Color4").style.backgroundColor = color4;
+    document.getElementById("Color5").style.backgroundColor = color5;
+    document.getElementById("Color6").style.backgroundColor = color6;
+    const ans = Math.floor(Math.random()*6);
+    colorans = colorshard[ans];
+    check();
 }
 
 function check(){
 
 }
+
+//return "rgb(" + r +", " + g + ", " + b +")";
